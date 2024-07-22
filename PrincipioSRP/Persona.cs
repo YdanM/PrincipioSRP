d133a2769d6d8f94751beb03c2f2d49187529726
YdanM <162137586+YdanM@users.noreply.github.com>
2024-07-22 13:13:00 -0400

    Agregar archivos de proyecto.

---
+++ PrincipioSRP/Persona.cs
@@ -0,0 +1,58 @@
+﻿
+
+using System;
+using System.Runtime.InteropServices;
+
+namespace PrincipioSRP
+{
+    public class Persona
+    {
+        public string nombre;
+        public int edad;
+        public string dirrecion;
+        public string correoElectronico;
+
+        public Persona(string nombre, int edad, string direccion, string correoElectronico)
+        {
+            this.nombre = nombre;
+            this.edad = edad;
+            this.dirrecion = direccion;
+            this.correoElectronico = correoElectronico;
+        }
+    }
+
+    public class EnviarCorreo
+    {
+        public string message;
+        public string receptor;
+
+        public EnviarCorreo(string message, string receptor)
+        {
+            this.message = message;
+            this.receptor = receptor;
+        }
+    }
+
+    public class ImprimirDatos
+    {
+        private Persona _persona;
+        private EnviarCorreo _enviarCorreo;
+
+        public ImprimirDatos(Persona persona, EnviarCorreo enviarCorreo)
+        {
+            _persona = persona;
+            _enviarCorreo = enviarCorreo;
+        }
+
+        public void Imprimir()
+        {
+            Console.WriteLine($"Nombre: {_persona.nombre}");
+            Console.WriteLine($"Edad: {_persona.edad}");
+            Console.WriteLine($"Dirección: {_persona.dirrecion}");
+            Console.WriteLine($"Correo Electronico: {_persona.correoElectronico}\n");
+            Console.WriteLine($"Receptor: {_enviarCorreo.receptor}");
+            Console.WriteLine($"mensaje: {_enviarCorreo.message}");
+
+        }
+    }
+}
---
+++ PrincipioSRP/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrincipioSRP
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona persona = new Persona("Ydan",19,"C/Sombrero","Idanmonegro@gmail.com");
+
+            EnviarCorreo enviar = new EnviarCorreo("Esta es la tarea de programación","Francis Ramirez");
+
+            ImprimirDatos datos = new ImprimirDatos(persona, enviar);
+            datos.Imprimir();
+            Console.ReadKey();
+        }
+    }
+}
